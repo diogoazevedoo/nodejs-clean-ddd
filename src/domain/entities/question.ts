0d188c87bf596bc24fde4d8f6f977e0adbaa3a6b
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-13 17:28:01 +0100

    feat: add slug value object

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "node:crypto"
+import type { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
     authorId: string
     title: string
+    slug: Slug
     content: string
 }
 
@@ -10,11 +12,13 @@ export class Question {
     public id: string
     public authorId: string
     public title: string
+    public slug: Slug
     public content: string   
 
     constructor(props: QuestionProps, id?: string) {
         this.authorId = props.authorId
         this.title = props.title
+        this.slug = props.slug
         this.content = props.content
         this.id = id ?? randomUUID()
     }
