b8d58af82a565f7ba73d93122bbd937917a5a474
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-15 14:02:08 +0100

    lint: add and run lint

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -3,19 +3,17 @@ import type { AnswersRepository } from '../repositories/answers-repository'
 import type { Answer } from '../entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
-    create: async (answer: Answer) => {
-        return
-    }
+  create: async (answer: Answer) => {},
 }
 
 test('create an answer', async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
-    const answer = await answerQuestion.execute({
-        instructorId: '1',
-        questionId: '1',  
-        content: 'New response',
-    })
+  const answer = await answerQuestion.execute({
+    instructorId: '1',
+    questionId: '1',
+    content: 'New response',
+  })
 
-    expect(answer.content).toEqual('New response')
+  expect(answer.content).toEqual('New response')
 })
